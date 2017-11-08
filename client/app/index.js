1c0085cfdfbabe899e14c60499ee2ec84404a0a4
mtorres <migueltorres@orange.fr>
2017-11-08 22:36:56 +0100

    CREATION DE NOTRE PREMIER COMPOSANT REACT

--- client/app/index.js
+++ client/app/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/style.css';
+
+import Dashboard from './modules/dashboard/components/dashboard';
+
+const App = () => {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
